ff9cf2dbe45d90f786b488a9d30dc035eb06ff87
Jindřich Káňa <jindrich.kana@elostech.cz>
2018-04-02 23:57:42 +0200

    oprava skriptu install.sh

--- mariadb-install.sh
+++ mariadb-install.sh
@@ -1,16 +1,18 @@
-#!/bin/bash
+#!/bin/sh
 
 chown -R mysql: /var/lib/mysql
 
-mysql_install_db --user mysql &
+mysql_install_db --user=mysql &
 
 sleep 10s
+
+mysqld_safe
 
 mysql -v < /tmp/mariadb-setup.sql
 
-kill -9 $(ps axu | grep mysql | grep -v | awk '{ print $2 }')
+kill -9 $(ps axu | grep -v grep | grep mysql | awk '{ print $2}')
 
-sleep 10s
+sleep 5s
 
-mysqld_safe --user mysql
+mysqld_safe --user=mysql
 
