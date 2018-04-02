3c4e152928b9ce5e023ef572e786c255ae98e16b
Jindřich Káňa <jindrich.kana@elostech.cz>
2018-04-02 17:06:04 +0200

    druhý push, oprava sql souboru

--- mariadb-setup.sql
+++ mariadb-setup.sql
@@ -1,14 +1,14 @@
 CREATE DATABASE reviews;
 
-USE REVIEWS;
+USE reviews;
 
 CREATE TABLE user_review (
-  id MEDIUMINT NOT NULL AUTO_INCREMENT PRIMARZ KEY,
+  id MEDIUMINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   reviewer_name CHAR(100),
   star_rating TINYINT,
   details VARCHAR(4000)
   );
 
-GRANT ALL PRIVILEGES ON *.* TO 'hodnoceni'@'%' WITH GRANT OPTION;
-UPDATE user SET password=PASSWORD("hodnoceni") WHERE user='hodnoceni';
+GRANT ALL ON reviews.* to 'hodnoceni'@'localhost' IDENTIFIED BY 'hodnoceni';
+GRANT ALL ON reviews.* to 'hodnoceni'@'%' IDENTIFIED BY 'hodnoceni';
 FLUSH PRIVILEGES;
